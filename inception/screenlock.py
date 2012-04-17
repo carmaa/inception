cd838fc2b02faa054c3958e9696ee04087987bff
Carsten Maartmann-Moe <carsten@carmaa.com>
2012-04-16 20:02:43 -0400

    Changed start address back to 1 MiB. Too much disparate results with accessing the upper memory region

--- inception/screenlock.py
+++ inception/screenlock.py
@@ -125,6 +125,7 @@ def searchanddestroy(device, target, memsize):
     Main search loop
     '''
     pageaddress = settings.startaddress
+    print(pageaddress)
     signatures = target['signatures']
 
     # Add signature lengths in bytes to the dictionary, and replace integer

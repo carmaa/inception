69bb3f5e7aa519151b28ebdf308782dea44990f2
Carsten Maartmann-Moe <carsten@carmaa.com>
2012-04-25 16:40:39 -0400

    Added licencing text to source

--- inception/firewire.py
+++ inception/firewire.py
@@ -1,4 +1,22 @@
 '''
+Inception - a FireWire physical memory manipulation and hacking tool exploiting
+IEEE 1394 SBP-2 DMA.
+
+Copyright (C) 2012  Carsten Maartmann-Moe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Created on Jan 23, 2012
 
 @author: Carsten Maartmann-Moe <carsten@carmaa.com> aka ntropy <n@tropy.org>
@@ -196,3 +214,4 @@ class FireWire:
         The list of vendors
         """
         return self._vendors
+

e186c56c0a17ce1e6472fb85d2641a40abae978e
Carsten Maartmann-Moe <carsten@carmaa.com>
2012-04-29 19:02:19 -0400

    Firewire unit test

---
+++ inception/firewire_test.py
@@ -0,0 +1,49 @@
+'''
+Inception - a FireWire physical memory manipulation and hacking tool exploiting
+IEEE 1394 SBP-2 DMA.
+
+Copyright (C) 2012  Carsten Maartmann-Moe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Created on Jan 30, 2012
+
+@author: Carsten Maartmann-Moe <carsten@carmaa.com> aka ntropy <n@tropy.org>
+'''
+import unittest
+from inception.firewire import FireWire
+
+
+class TestUtil(unittest.TestCase):
+
+
+    def setUp(self):
+        self.fw = FireWire()
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_init_OUI(self):
+        self.assertIsInstance(self.fw.oui, dict)
+        # Test a couple of OUIs
+        self.assertEqual(self.fw.resolve_oui(0x03), 'XEROX CORPORATION')
+        self.assertEqual(self.fw.resolve_oui(0xE0C1), 'MEMOREX TELEX JAPAN, LTD.')
+        self.assertEqual(self.fw.resolve_oui(0xFCFBFB), 'Cisco Systems')
+    
+
+
+if __name__ == "__main__":
+    unittest.main()

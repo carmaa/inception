35e64b16e02cabf2f166a1d8c760e71b8e0f7499
blunden <blunden2@gmail.com>
2016-08-15 18:36:11 +0200

    Update some Windows 7 signatures
    Adds support for:
    
    6.1.7601.23418
    6.1.7601.23452
    6.1.7601.23455

--- inception/modules/unlock.py
+++ inception/modules/unlock.py
@@ -170,7 +170,7 @@ targets = [
                 version=None,
                 md5=None,
                 tag=False,
-                offsets=[0x2a8, 0x2a1, 0x291, 0x321, 0xe59,0xe71,0xe09],
+                offsets=[0x2a8, 0x2a1, 0x291, 0x321, 0xe59,0xe71,0xe09,0xdf1],
                 chunks=[
                     Chunk(
                         chunk=0xc60f85,
